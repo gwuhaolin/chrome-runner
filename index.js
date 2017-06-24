7e43cf4187337b3a38ed037597743fbb73a33bc8
halwu(吴浩麟) <halwu@tencent.com>
2017-06-24 19:05:51 +0800

    add .travis.yml

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const chromeFinder = require('./chrome-finder');
 const DEFAULT_FLAGS = require('./flags');
 const { getRandomPort, makeTmpDir, delay, isPortOpen } = require('./util');
 
+if (!global.logger) {
+  global.logger = console;
+}
+
 const { spawn, execSync } = childProcess;
 const isWindows = process.platform === 'win32';
 const SUPPORTED_PLATFORMS = new Set(['darwin', 'linux', 'win32']);
@@ -16,7 +20,7 @@ const SUPPORTED_PLATFORMS = new Set(['darwin', 'linux', 'win32']);
 class Runner {
   constructor(opts = {}) {
     this.tmpDirandPidFileReady = false;
-    this.chrome = undefined;
+    this.chromeProcess = undefined;
     this.chromeDataDir = undefined;
     this.chromeOutFile = undefined;
     this.chromeErrorFile = undefined;
@@ -33,7 +37,7 @@ class Runner {
       try {
         return await this.isDebugReady();
       } catch (err) {
-        console.log('ChromeRunner', `No chrome found on port ${this.port}, launching a new Chrome.`);
+        global.logger.warn(`no chrome found on port ${this.port}, launching a new Chrome.`);
       }
     }
 
@@ -43,10 +47,17 @@ class Runner {
 
     if (!this.chromePath) {
       const installations = await chromeFinder[process.platform]();
-      if (installations.length === 0) {
-        throw new Error('No Chrome Installations Found');
+      let chromePath;
+      if (installations.length > 0) {
+        chromePath = installations[0];
+      } else if (process.env['CHROME_PATH']) {
+        chromePath = process.env['CHROME_PATH'];
       }
-      this.chromePath = installations[0];
+      if (chromePath) {
+        this.chromePath = chromePath;
+      } else {
+        throw new Error('no chrome installations found, if you has install chrome set env CHROME_PATH to your chrome exe path');
+      }
     }
 
     await this.spawn(this.chromePath);
@@ -56,24 +67,24 @@ class Runner {
   kill() {
     this.restartUnexpectedChrome = false;
     return new Promise(resolve => {
-      if (this.chrome) {
-        this.chrome.on('close', () => {
+      if (this.chromeProcess) {
+        this.chromeProcess.on('close', () => {
           this.destroyTmp();
           resolve();
         });
 
-        console.log('ChromeRunner', 'Killing all Chrome Instances');
+        global.logger.info('killing all Chrome Instances');
         try {
           if (isWindows) {
-            execSync(`taskkill /pid ${this.chrome.pid} /T /F`);
+            execSync(`taskkill /pid ${this.chromeProcess.pid} /T /F`);
           } else {
-            process.kill(-this.chrome.pid);
+            process.kill(-this.chromeProcess.pid);
           }
         } catch (err) {
-          console.error('ChromeRunner', `Chrome could not be killed ${err.message}`);
+          global.logger.error(`chrome could not be killed ${err.message}`);
         }
 
-        delete this.chrome;
+        delete this.chromeProcess;
       } else {
         // fail silently as we did not start chrome
         resolve();
@@ -100,7 +111,7 @@ class Runner {
   prepare() {
     const platform = process.platform;
     if (!SUPPORTED_PLATFORMS.has(platform)) {
-      throw new Error(`Platform ${platform} is not supported`);
+      throw new Error(`platform ${platform} is not supported`);
     }
 
     this.chromeDataDir = makeTmpDir();
@@ -108,32 +119,32 @@ class Runner {
     this.chromeErrorFile = fs.openSync(`${this.chromeDataDir}/chrome-err.log`, 'a');
     this.pidFile = fs.openSync(`${this.chromeDataDir}/chrome.pid`, 'a');
 
-    console.info('ChromeRunner', `Created chrome data dir ${this.chromeDataDir}`);
+    global.logger.info(`created chrome data dir ${this.chromeDataDir}`);
 
     this.tmpDirandPidFileReady = true;
   }
 
   async spawn() {
-    if (this.chrome) {
-      console.info('ChromeRunner', `Chrome already running with pid ${this.chrome.pid}.`);
-      return this.chrome.pid;
+    if (this.chromeProcess) {
+      global.logger.info(`chrome already running with pid ${this.chromeProcess.pid}.`);
+      return this.chromeProcess.pid;
     }
 
     if (!this.port) {
       this.port = await getRandomPort();
     }
-    const chrome = spawn(this.chromePath, this.flags, {
+    const chromeProcess = spawn(this.chromePath, this.flags, {
       detached: true,
       stdio: ['ignore', this.chromeOutFile, this.chromeErrorFile]
     });
-    this.chrome = chrome;
+    this.chromeProcess = chromeProcess;
     this.handleSIGINT();
     this.handleChromeUnexpectedExit();
-    fs.writeFileSync(this.pidFile, chrome.pid.toString());
-    console.info('ChromeRunner', `Chrome running with pid ${chrome.pid} on port ${this.port}.`);
+    fs.writeFileSync(this.pidFile, chromeProcess.pid.toString());
+    global.logger.info(`chrome running with pid ${chromeProcess.pid} on port ${this.port}.`);
 
     await this.waitUntilReady();
-    return chrome.pid;
+    return chromeProcess.pid;
   }
 
   handleSIGINT() {
@@ -144,11 +155,11 @@ class Runner {
   }
 
   handleChromeUnexpectedExit() {
-    if (this.chrome) {
-      this.chrome.on('close', async () => {
+    if (this.chromeProcess) {
+      this.chromeProcess.on('close', async () => {
         if (this.restartUnexpectedChrome) {
-          console.warn('ChromeRunner', 'Chrome exit unexpected, restart it');
-          delete this.chrome;
+          global.logger.warn('chrome exit unexpected, restart it');
+          delete this.chromeProcess;
           await this.spawn();
         }
       })
@@ -161,13 +172,13 @@ class Runner {
 
   // resolves when debugger is ready, rejects after 10 polls
   waitUntilReady() {
-    console.info('ChromeRunner', 'Wait until chrome ready');
+    global.logger.info('wait until chrome ready');
     return new Promise((resolve, reject) => {
       let retries = 0;
       const poll = () => {
         retries++;
         this.isDebugReady().then(() => {
-          console.info('ChromeRunner', 'Chrome already now');
+          global.logger.info('chrome already now');
           resolve();
         }).catch(async (err) => {
           if (retries > 10) {
--- test/runner.test.js
+++ test/runner.test.js
@@ -5,20 +5,25 @@ const Runner = require('../index');
 
 process.on('unhandledRejection', console.trace);
 
-describe('Runner', () => {
+describe('Runner', function () {
 
-  it('launch() kill()', async () => {
+  it('launch() then kill()', async function () {
     const runner = new Runner();
     await runner.launch();
-    assert.notEqual(runner.chrome, undefined);
-    await runner.kill();
+    assert.notEqual(runner.chromeProcess, null);
+    return await runner.kill();
   });
 
-  it('restartUnexpectedChrome', async function () {
-    this.timeout(30000);
+  it('restart chrome when chrome exit unexpected', async function () {
+    this.timeout(5000);
     const runner = new Runner();
     await runner.launch();
-    await delay(20000);
+    process.kill(runner.chromeProcess.pid);
+    await delay(1000);
+    process.kill(runner.chromeProcess.pid);
+    await delay(1000);
+    process.kill(runner.chromeProcess.pid);
+    await delay(1000);
     await runner.kill();
   });
 
